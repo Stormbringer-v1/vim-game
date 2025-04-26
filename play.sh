75a53b65774e7fb3876ba317803e677600723ae2
Robert Harutyunyan <harutyunyan341@gmail.com>
2025-04-26 13:22:32 +0400

    levels 4 and 5

--- play.sh
+++ play.sh
@@ -31,6 +31,33 @@ run_level() {
         echo "level2=completed" >> progress.log
       fi
       ;;
+    3)
+      TASK_CONTENT=$(awk '/<<TASK>>/{flag=1;next}/<<END>>/{flag=0}flag' "$TMP_FILE")
+      if echo "$TASK_CONTENT" | grep -q "MISTAEK"; then
+        echo "❌  Mission failed: The typo is still there. Try again."
+      else
+        echo "🎉  Mission accomplished! You passed Level 3!"
+        echo "level3=completed" >> progress.log
+      fi
+      ;;
+        4)
+      TASK_CONTENT=$(awk '/<<TASK>>/{flag=1;next}/<<END>>/{flag=0}flag' "$TMP_FILE")
+      if echo "$TASK_CONTENT" | grep -q "FAILD"; then
+        echo "❌  Mission failed: The typo is still there. Try again."
+      else
+        echo "🎉  Mission accomplished! You passed Level 4!"
+        echo "level4=completed" >> progress.log
+      fi
+      ;;
+        5)
+      TASK_CONTENT=$(awk '/<<TASK>>/{flag=1;next}/<<END>>/{flag=0}flag' "$TMP_FILE")
+      if echo "$TASK_CONTENT" | grep -q "sdklfjskldfj"; then
+        echo "❌  Mission failed: You didn't clean up the unwanted line."
+      else
+        echo "🎉  Mission accomplished! You passed Level 5!"
+        echo "level5=completed" >> progress.log
+      fi
+      ;;
     *)
       echo "Unknown level!"
       ;;
@@ -44,6 +71,12 @@ if ! grep -q "level1=completed" progress.log; then
   run_level 1
 elif ! grep -q "level2=completed" progress.log; then
   run_level 2
+elif ! grep -q "level3=completed" progress.log; then
+  run_level 3
+  elif ! grep -q "level4=completed" progress.log; then
+  run_level 4
+elif ! grep -q "level5=completed" progress.log; then
+  run_level 5
 else
   echo "🎉 Congratulations! You finished all available levels!"
 fi
